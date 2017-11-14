4294ea33ca2111f74c9a0d81565183b1fba97462
Simon Lee <simonhlee97@hotmail.com>
2017-11-13 20:50:38 -0500

    ready for another deploy

--- src/app/goteamapp/goteamapp.component.ts
+++ src/app/goteamapp/goteamapp.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { fade } from './../animations';
+
 import { SelectItem } from 'primeng/primeng';
 import { DropdownModule } from 'primeng/primeng';
 import {trigger,state,style,transition,animate,keyframes} from '@angular/animations';
@@ -13,35 +13,22 @@ interface Ticket {
 @Component({
   selector: 'goteamapp',
   templateUrl: './goteamapp.component.html',
-  styleUrls: ['./goteamapp.component.css'],
-  animations: [
-    fade
-    // trigger('fade', [
-    //   transition('void => *', [
-    //     style({opacity: 0}),
-    //     animate(2000)  // style({backgroundColor: 'white', opacity: 1}))  can clean up code because Default state is opacity=1 and bgColor is white
-    //   ]),
-    //   transition('* => void', [
-    //     animate(2000, style({opacity: 0}))
-    //   ])
-    ]
+  styleUrls: ['./goteamapp.component.css']
 })
 export class GoteamappComponent implements OnInit {
 
-  types: SelectItem[];
+  teams: SelectItem[];
   selectedType: string;
-
 
   seat: Ticket[];
   selection: Ticket;
 
-
   question = 'Which team will make the first 3-pointer in the 4th quarter?';
   prize = 'Pacers t-shirt';
-  // animateMe() {
-  //   this.state = (this.state === 'one' ? 'two' : 'one');
-  // }
+
+  showHide: boolean;
   constructor() { 
+    this.showHide = true;
     this.seat = [
       { name: '101', code: '101' },
       { name: '102', code: '102' },
@@ -49,14 +36,17 @@ export class GoteamappComponent implements OnInit {
       { name: '104', code: '104' },
       { name: '105', code: '105' },
       { name: '106', code: '106' },
-      { name: '107', code: '107' },
-      { name: '108', code: '108' }
+      { name: '107', code: '107' }
     ],
-    this.types = [];
-    this.types.push({ label: 'Pacers', value: 'Pacers' });
-    this.types.push({ label: 'Celtics', value: 'Celtics' });
+    this.teams = [];
+    this.teams.push({ label: 'Pacers', value: 'Pacers' });
+    this.teams.push({ label: 'Celtics', value: 'Celtics' });
    }
-
+  switchdiv(){
+    // hide #goteam and display #play
+    this.showHide = !this.showHide;
+  }
+  
   ngOnInit() {
     
   }
